3411c98d52328ac7f3901d61e9d84d116ded09ab
SK ABIR ALI <219447337+bhittu21@users.noreply.github.com>
2026-05-13 18:57:25 +0600

    Refactor robots rules to use array format
    Allowed Screaming Frog SEO Spider Only.
    
    Needed for Technical SEO

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,9 +2,15 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      disallow: "/",
-    },
+    rules: [
+      {
+        userAgent: "Screaming Frog SEO Spider",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/",
+      },
+    ],
   };
 }
