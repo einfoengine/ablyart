88fe1de4ee5ef4920c90203374e28fc1a8044110
bhittu21 <219447337+bhittu21@users.noreply.github.com>
2026-06-07 20:56:31 +0600

    Removed Client Success from Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import GrowthCalculatorSection from "@/components/GrowthCalculatorSection";
 import HotServicesSection from "@/components/HotServicesSection";
-import VslSection from "@/components/VslSection";
 import ServicesSection from "@/components/ServicesSection";
-import CompanyLogosSection from "@/components/CompanyLogosSection";
 import WhyChooseUsSection from "@/components/WhyChooseUsSection";
 import TechAndSocial from "@/components/TechAndSocial";
 import ValueFocusSection from "@/components/ValueFocusSection";
 import BlogSection from "@/components/BlogSection";
 import AgitationSection from "@/components/AgitationSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
 import FaqSection from "@/components/FaqSection";
 
 
@@ -27,7 +24,6 @@ export default function Home() {
         <AgitationSection id="agitation-section" />
         {/* <CompanyLogosSection id="company-logos-section" /> */}
         <WhyChooseUsSection id="why-choose-us-section" />
-        <TestimonialsSection id="testimonials-section" />
         <TechAndSocial id="tech-and-social-section" />
         <FaqSection id="faq-section" />
         <BlogSection id="blog-section" />
